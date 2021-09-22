a1a23df1945431929164abfe9f1a4eea897a7378
amipedro <ami_pedro@hotmail.com>
2021-09-22 11:32:15 -0300

    Edit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("wiki/<str:entry>", views.entry, name='entry'),
     path("search", views.search, name='search'),
-    path("create", views.create, name='create')
+    path("create", views.create, name='create'),
+    path("edit", views.edit, name='edit')
 ]
